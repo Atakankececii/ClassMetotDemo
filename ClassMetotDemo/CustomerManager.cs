dfd1ff3103d8e04704085508ee739a56128d9aff
Süleyman Atakan Keçeci <atakan_kececi48@hotmail.com>
2021-01-17 17:19:27 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        
+       
+    
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Eklenen müşterinin adı :" + customer.Name +"\n"+"Eklenen müşterinin soyadı :"+customer.Surname);
+           
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Silinen müşterinin adı :" + customer.Name + "\n" + "Silinen müşterinin soyadı :" + customer.Surname);
+
+        }
+
+        public void Show(Customer customer)
+        {
+            Console.WriteLine("Müşterinin Id numarası :"+customer.Id+"\n"+"Müşterinin adı:"+customer.Name+"\n"+"Müşterinin soyadı:"+customer.Surname);
+        }
+
+       
+        public void ShowasList(Customer[] customers)
+        {
+            
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Müşterinin adı : "+customer.Name+"\n"+"Müşterinin soyadı:"+customer.Surname+"\n"+"----------------");
+
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Atakan";
+            customer1.Surname = "Keçeci";
+
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Mehmet";
+            customer2.Surname = "Keçeci";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Name = "Sevilay";
+            customer3.Surname = "Keçeci";
+
+
+            
+            Customer[] customers = new Customer[] { customer1,customer2,customer3};
+
+
+            CustomerManager customermanager = new CustomerManager();
+            //customermanager.Add(customer3);
+            //customermanager.Delete(customer3);
+            //customermanager.Show(customer1);
+            customermanager.ShowasList(customers);
+        }
+    }
+}
